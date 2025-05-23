b8a1710197953062598beb1ec2ad90e8db1edb6e
joesanthosh24 <joe.santhosh88@yahoo.com>
2025-05-23 10:38:17 -0400

    Added sign up page and sign up form. Fixed layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Navbar from "@/components/navbar";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-     <Navbar />
-    </main>
+    <>
+      
+    </>
   );
 }
